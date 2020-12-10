8736d5ae52a436ea4cebbaa69adae766fffaeee3
Mark Bluemer <mark.bluemer@pillpack.com>
2020-12-09 21:40:04 -0500

    Clean out work-specific items
    Move all work-specific fish configuration to a files that's
    not tracked.

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -9,16 +9,19 @@ alias wiki="nvim ~/vimwiki/index.md"
 alias cp="cp -i"
 alias mv="mv -i"
 
-# Python virtualenvironment activation
+#### Python
 #alias venv='source /home/$USER/code/virtualenvs/${PWD##*/}/bin/activate.fish'
+alias ptr='poetry run'
 
 #### GIT
 alias gs="git status"
 alias ga="git add"
 alias gc="git commit -m"
+alias cb="git branch | fzf --header Checkout | xargs git checkout"
 alias gpull="git pull"
 alias gpush="git push"
 alias gpom="git push origin master"
+alias git-branch-clean="git branch --merged | egrep -v \"(^\*|master|dev)\" | xargs git branch -d"
 
 #### Rust
 alias cr="cargo run"
