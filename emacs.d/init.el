813aa70f5a99657f1efe982f2ee33e13836b80a5
Mark Bluemer <mark@theblue.dev>
2020-12-25 21:54:14 -0500

    Make Org mode look nicer
    Add configuration to make Org mode look nicer and
    reformat the existing `init.org` code.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,17 +1,25 @@
 (if (display-graphic-p)
-(progn
-(set-fringe-mode 10)
-(scroll-bar-mode -1)))
+    (progn
+      (set-fringe-mode 10)
+      (scroll-bar-mode -1)))
 (tooltip-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 
 (setq backup-directory-alist `(("." . "~/.saves")))
 
+(defvar mb/default-font-size 120)
+(defvar mb/default-variable-font-size 140)
+
+(set-face-attribute 'default nil :font "FiraCode Nerd Font Mono" :height mb/default-font-size)
+(set-face-attribute 'fixed-pitch nil :font "FiraCode Nerd Font Mono" :height mb/default-font-size)
+(set-face-attribute 'variable-pitch nil :font "Georgia" :height mb/default-variable-font-size :weight 'regular)
+
 (require 'package)
 
 (setq package-archives '(("melpa" . "http://melpa.org/packages/")
-			 ("melpa-stable" . "http://stable.melpa.org/packages/")))
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("melpa-stable" . "http://stable.melpa.org/packages/")))
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -22,21 +30,6 @@
 
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
-
-(use-package evil
-  :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  (setq evil-want-C-u-scroll t)
-  :config
-  (evil-mode 1))
-(use-package evil-collection
-  :after evil
-  :ensure t
-  :config
-  (evil-collection-init))
-
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 (use-package general
   :config
@@ -53,6 +46,25 @@
 (use-package which-key
   :init (which-key-mode))
 
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  :config
+  (evil-mode 1))
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(mb/evil-leader-key-def
+  "w" '(write-file :which-key "Write file")
+  "c" '(evil-window-delete :which-key "Delete window"))
+
 (use-package helpful
   :custom
   (counsel-describe-function-function #'helpful-callable)
@@ -64,6 +76,7 @@
   ([remap describe-key] . helpful-key))
 
 (use-package ivy
+  :bind (("C-s" . swiper))
   :config
   (ivy-mode 1))
 
@@ -91,19 +104,20 @@
      (t . 10)))
   (ivy-posframe-display-functions-alist
    '((complete-symbol . ivy-posframe-display-at-point)
-     (swiper . nil)
-     (swiper-isearch . nil)
+     (swiper . ivy-display-function-fallback)
+     (swiper-isearch . ivy-display-function-fallback)
      (t . ivy-posframe-display-at-frame-center)))
   :config
   (ivy-posframe-mode 1))
 
 (mb/leader-key-def
- "x" '(counsel-M-x :which-key "Counsel execute function")
- ;; File completions
- "ff" '(counsel-find-file :which-key "Counsel find file")
- "fd" '(delete-file :which-key "Delete file")
- ;; Buffer commands
- "bb" '(counsel-ibuffer :which-key "Counsel switch buffer"))
+  "x" '(counsel-M-x :which-key "Counsel execute function")
+  ;; File completions
+  "ff" '(counsel-find-file :which-key "Counsel find file")
+  "fd" '(delete-file :which-key "Delete file")
+  ;; Buffer commands
+  "bb" '(counsel-ibuffer :which-key "Counsel switch buffer")
+  "bl" '(evil-switch-to-windows-last-buffer :which-key "Switch to last buffer"))
 
 (use-package ripgrep)
 
@@ -125,7 +139,8 @@
 (column-number-mode)
 (global-display-line-numbers-mode t)
 
-(dolist (mode '(eshell-mode-hook))
+(dolist (mode '(eshell-mode-hook
+                org-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 (use-package yaml-mode)
@@ -133,12 +148,12 @@
 (use-package dockerfile-mode
   :config
   (add-to-list 'auto-mode-alist
-	       '("Dockerfile\\'" . dockerfile-mode)))
+               '("Dockerfile\\'" . dockerfile-mode)))
 
 (use-package magit
   :init
   (setq magit-repository-directories '(("~/code/" . 3)
-				       ("~/.dotfiles/" . 1))))
+                                       ("~/.dotfiles/" . 1))))
 
 (use-package projectile
   :after magit
@@ -153,10 +168,77 @@
   :init
   (setq projectile-switch-project-action #'projectile-dired)
   (mapc #'projectile-add-known-project
-          (mapcar #'file-name-as-directory (magit-list-repos)))
-    (projectile-save-known-projects))
+        (mapcar #'file-name-as-directory (magit-list-repos)))
+  (projectile-save-known-projects))
 (use-package counsel-projectile
   :config (counsel-projectile-mode))
 
 (mb/leader-key-def
- "p" '(projectile-command-map :which-key "Projectile commands"))
+  "p" '(projectile-command-map :which-key "Projectile commands"))
+
+(setq org-structure-template-alist
+      '(("a" . "export ascii")
+        ("c" . "center")
+        ("C" . "comment")
+        ("e" . "example")
+        ("E" . "export")
+        ("h" . "export html")
+        ("l" . "src emacs-lisp")
+        ("p" . "src python")
+        ("q" . "quote")
+        ("s" . "src")
+        ("v" . "verse")))
+
+(defun mb/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Georgia" :weight 'regular :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
+  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch))
+
+(defun mb/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+(use-package org
+  :pin manual
+  :hook (org-mode . mb/org-mode-setup)
+  :config
+  (require 'org-tempo)
+  (mb/org-font-setup)
+  :custom
+  (org-ellipsis " ▾")
+  (org-src-tab-acts-natively t)
+  (org-hide-emphasis-markers t))
+
+(use-package org-bullets
+  :hook (org-mode . org-bullets-mode))
+
+(defun mb/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . mb/org-mode-visual-fill))
