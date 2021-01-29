b224489826e5f64e2897399271ab8d1019181722
Mark Bluemer <mark@theblue.dev>
2021-01-29 16:20:24 -0500

    Update some fish config

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,13 @@ starship init fish | source
 if test -e ~/.asdf
     source ~/.asdf/asdf.fish
 end
+
+
+if type -q pyenv
+    pyenv init - | source
+    pyenv virtualenv-init - | source
+end
+
+if type -q direnv
+    direnv hook fish | source
+end
