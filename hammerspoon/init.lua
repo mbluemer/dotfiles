1093ba31d68a952d5f4d39e46f58fea7af666986
Mark Bluemer <mbluemer@amazon.com>
2022-01-05 10:52:18 -0500

    Cleanup; add shortcuts; add emoji selector

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,12 +1,46 @@
 -- https://www.mortensoncreative.com/blog/break-up-with-your-mouse-2
 hs.loadSpoon("ModalMgr")
-local K = require('keys')
 
 spoon.ModalMgr:new("hyperM")
 local hyper_modal = spoon.ModalMgr.modal_list["hyperM"]
 
+--# EMOJI SELECTOR #--
+-- https://aldur.github.io/articles/hammerspoon-emojis/
+
+-- Build the list of emojis to be displayed.
+local choices = {}
+for _, emoji in ipairs(hs.json.decode(io.open("emojis/emojis.json"):read())) do
+    table.insert(choices,
+	{text=emoji['name'],
+	    subText=table.concat(emoji['kwds'], ", "),
+	    image=hs.image.imageFromPath("emojis/" .. emoji['id'] .. ".png"),
+	    chars=emoji['chars']
+	})
+end
+
+-- Focus the last used window.
+local function focusLastFocused()
+    local wf = hs.window.filter
+    local lastFocused = wf.defaultCurrentSpace:getWindows(wf.sortByFocusedLast)
+    if #lastFocused > 0 then lastFocused[1]:focus() end
+end
+
+-- Create the chooser.
+-- On selection, copy the emoji and type it into the focused application.
+local chooser = hs.chooser.new(function(choice)
+    if not choice then focusLastFocused(); return end
+    hs.pasteboard.setContents(choice["chars"])
+    focusLastFocused()
+    hs.eventtap.keyStrokes(hs.pasteboard.getContents())
+end)
+
+chooser:searchSubText(true)
+chooser:choices(choices)
+
 -- TODO Tiered modes? For example W to trigger window mode with it's own bindings
--- TODO Switch through multiple windows of an application
+-- TODO Switch through multiple windows of an application (?)
+-- Maybe that's: if already on the application switch to a different window, add
+-- shift modifier to jump through windows
 local BINDINGS = {
    {
       description = 'Exit',
@@ -16,8 +50,14 @@ local BINDINGS = {
    {
       description = 'Exit',
       key = 'g',
-      modifiers = {K.ctrl},
+      modifiers = {'ctrl'},
       onEnter = function() end,
+   },
+   {
+      description = 'Toggle cheat sheet',
+      key = '/',
+      onEnter = function() spoon.ModalMgr:toggleCheatsheet() end,
+      keepModalOpen = true,
    },
    {
       description = 'Reload config',
@@ -73,6 +113,13 @@ local BINDINGS = {
       end,
    },
    {
+      description = 'Emoji Selector',
+      key = '1',
+      onEnter = function()
+	 chooser:show()
+      end,
+   },
+   {
       description = '[F]irefox',
       key = 'F',
       onEnter = function() hs.application.launchOrFocus('Firefox') end,
@@ -97,6 +144,16 @@ local BINDINGS = {
       key = 'M',
       onEnter = function() hs.application.launchOrFocus('Messages') end,
    },
+   {
+      description = 'Amazon [C]hime',
+      key = 'C',
+      onEnter = function() hs.application.launchOrFocus('Amazon Chime') end,
+   },
+   {
+      description = 'S[p]otify',
+      key = 'P',
+      onEnter = function() hs.application.launchOrFocus('Spotify') end,
+   },
 }
 
 for _, binding in ipairs(BINDINGS) do
@@ -106,13 +163,15 @@ for _, binding in ipairs(BINDINGS) do
       binding.description,
       function ()
 	 binding.onEnter()
-	 spoon.ModalMgr:deactivate({"hyperM"})
+	 if not binding.keepModalOpen then
+	    spoon.ModalMgr:deactivate({"hyperM"})
+	 end
       end
    )
 end
 
-hs.hotkey.bind(K.ctrl, 'space', function()
+hs.hotkey.bind('ctrl', 'space', function()
    spoon.ModalMgr:deactivateAll()
    -- Show the keybindings cheatsheet once appM is activated
-   spoon.ModalMgr:activate({"hyperM"}, "#FFBD2E", true)
+   spoon.ModalMgr:activate({"hyperM"}, "#FFBD2E", false)
 end)
